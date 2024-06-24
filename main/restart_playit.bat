750b021912ccf263862bf54d0dae45b3c32fcc20
Dakari Mona <80322542+DakariMona@users.noreply.github.com>
2024-06-24 14:16:48 +0700

    Update restart_playit.bat

--- main/restart_playit.bat
+++ main/restart_playit.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
 cls
-ECHO Restarting Playit.gg.
-taskkill /F /IM playit.exe /T > NUL 2>&1
-ECHO Stop Playit.gg sucessfuly. Starting Playit.gg.
-start C:\"Program Files"\playit_gg\bin\playit.exe
+if exist C:\"Program Files"\playit_gg\bin\playit.exe (
+  ECHO Restarting Playit.gg.
+  taskkill /F /IM playit.exe /T > NUL 2>&1
+  ECHO Stop Playit.gg sucessfuly. Starting Playit.gg.
+  start C:\"Program Files"\playit_gg\bin\playit.exe
+) else (
+  echo No Playit.gg found in .
+)
 TIMEOUT /T 5
 call menu.bat
