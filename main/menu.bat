8b4f081ac1c9efcba25210f58d7c80eeb7a54e6f
Dakari Mona <80322542+DakariMona@users.noreply.github.com>
2024-06-24 14:27:57 +0700

    Update menu.bat

--- main/menu.bat
+++ main/menu.bat
@@ -8,7 +8,11 @@ echo --{ Minecraft Server Manager }--
 echo.
 
 echo 1. Start Minecraft Server
-echo 2. Restart Playit.gg
+if exist %choice%\fabric.jar (
+  echo 2. Restart Playit.gg
+) else (
+  echo 2. Restart Playit.gg (unable)
+)
 echo 3. Stop Java Processes
 echo 4. Reset Minecraft World (WARNING: Deletes existing world!)
 echo 5. Exit
@@ -22,11 +26,30 @@ if "%choice%"=="" (
   goto invalid_choice
 )
 
-if /i "%choice%" LEQ 6 & /i "%choice%" GEQ 1 (
-  call "%choice%"%.bat
-  goto menu
-) else (
-  goto invalid_choice
+if "%choice%"=="1"(
+  call start.bat
+)
+
+if "%choice%"=="2"(
+  if exist C:\"Program Files"\playit_gg\bin\playit.exe (
+    call restart_playit.bat
+  )
+)
+
+if "%choice%"=="3"(
+  call stop_java.bat
+)
+
+if "%choice%"=="4"(
+  call reset_world.bat
+)
+
+if "%choice%"=="5"(
+  exit
+)
+
+if "%choice%"=="6"(
+  call config.bat
 )
 
 :invalid_choice
